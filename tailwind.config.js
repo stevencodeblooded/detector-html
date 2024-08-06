d85cb7796531dd339cfef591cbbc5bc3305b8cd8
stevencodeblooded <steven.ochieng432@gmail.com>
2024-08-07 02:57:43 +0300

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ['./*.html', './js/**/*.js'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class',  // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
